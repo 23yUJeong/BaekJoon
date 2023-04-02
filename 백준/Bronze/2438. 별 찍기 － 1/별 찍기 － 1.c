a536dc3bed6d8d15b28ac5b3078e2baa3ae73ffd
서유정 <126013132+23yUJeong@users.noreply.github.com>
2023-04-02 22:17:13 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#define _CRT_SECURE_NO_WARNINGS
+
+int main() {
+  int n;
+
+  scanf("%d", &n);
+
+  for(int i = 1; i <= n; i++){
+    for(int j = 0; j < i; j++){
+      printf("*");
+    }
+    printf("\n");
+  }
+  
+  return 0;
+}
